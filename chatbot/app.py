d6a632d7e921a941f59d7dc338f47c51c59bd883
Yasir Mushtaq <46179498+yasir23@users.noreply.github.com>
2024-06-04 06:48:02 +0500

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -10,7 +10,6 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_chroma import Chroma
 import time
 import markdown
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
